d78b3a1ca2831888a69166641af92622462c3a13
Muhammad Iqbal Masykuri <muhammadiqbal@synapsis.id>
2023-11-17 06:16:21 +0700

    fix ws

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
-const cors = require("cors");
 const admin = require("firebase-admin");
 const serviceAccount = require("./capstonec04-c3cfc-firebase-adminsdk-uh2o4-9cd7371be2.json");
 
@@ -9,46 +5,60 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+const firestore = admin.firestore();
+const WebSocket = require("ws");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
 app.use(cors());
 
-const firestore = admin.firestore();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const clients = new Set();
 
+const ipv4Regex = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+
 wss.on("connection", async (ws, req) => {
-  const clientIp = req.connection.remoteAddress;
-  console.log(`Koneksi WebSocket baru dari ${clientIp}.`);
+  let clientIp = req.connection.remoteAddress;
+  console.log(`New WebSocket connection from ${clientIp}.`);
   clients.add(ws);
 
-  // Memeriksa apakah dokumen dengan nama clientIp sudah ada dalam koleksi "test-device"
+  if (clientIp.startsWith("::ffff:")) {
+    clientIp = clientIp.substring(7);
+  }
+
+  const isLocalhost = clientIp === "::1" || clientIp === "127.0.0.1";
+
   const deviceDoc = await firestore
     .collection("test-device")
     .doc(clientIp)
     .get();
 
   if (!deviceDoc.exists) {
-    console.log(
-      `Dokumen dengan nama ${clientIp} belum ada. Menambahkan dokumen.`
-    );
+    if (isLocalhost || ipv4Regex.test(clientIp)) {
+      console.log(
+        `Document with name ${clientIp} does not exist. Adding document.`
+      );
 
-    // Menambahkan dokumen pada koleksi "test-device" jika belum ada
-    const deviceCollection = firestore.collection("test-device").doc(clientIp);
+      const deviceCollection = firestore
+        .collection("test-device")
+        .doc(clientIp);
 
-    // Mengatur bidang (field) pada dokumen
-    await deviceCollection.set({
-      name: "",
-      wid: "",
-      gender: 0,
-    });
+      await deviceCollection.set({
+        name: "",
+        wid: "",
+        gender: 0,
+      });
+    } else {
+      console.log(`Create document error.`);
+    }
   } else {
-    console.log(`Dokumen dengan nama ${clientIp} sudah ada.`);
+    console.log(`Document with name ${clientIp} already exists.`);
   }
 
   ws.on("message", async (message) => {
-    console.log(`Pesan diterima: ${message}`);
-
     if (typeof message !== "string") {
       message = message.toString();
     }
@@ -61,22 +71,36 @@ wss.on("connection", async (ws, req) => {
       return;
     }
 
-    firestore.collection("pulse-data").doc(`${clientIp}`).add(data);
+    // Generate a timestamp and use it as the document ID
+    const timestamp = Math.floor(
+      admin.firestore.Timestamp.fromDate(new Date()).toMillis() / 1000
+    ).toString();
+
+    data.timestamp = timestamp;
+
+    console.log(`Received message: ${JSON.stringify(data)}`);
+
+    await firestore
+      .collection("pulse-data")
+      .doc(clientIp)
+      .collection("data")
+      .doc(timestamp)
+      .set(data);
 
     clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(JSON.stringify(data)); // send a JSON string
       }
     });
   });
 
   ws.on("close", () => {
-    console.log(`Koneksi WebSocket dari ${clientIp} ditutup.`);
+    console.log(`WebSocket connection from ${clientIp} closed.`);
     clients.delete(ws);
   });
 });
 
 const port = 5000;
 server.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
